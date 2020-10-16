21fb9b48bef6ca4ec6ea374ac6fda242caefdadc
Rohith M <rohith.m94@gmail.com>
2020-10-16 20:06:56 +0530

    react-router intial setup

---
+++ client-app/src/components/App.js
@@ -0,0 +1,27 @@
+import React, { Component } from "react";
+import { BrowserRouter, Route } from "react-router-dom";
+import Header from "./Header";
+import StreamCreate from "./streams/StreamCreate";
+import StreamEdit from "./streams/StreamEdit";
+import StreamList from "./streams/StreamList";
+import StreamShow from "./streams/StreamShow";
+import StreamDelete from "./streams/StremDelete";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="ui container">
+        <BrowserRouter>
+          <Header />
+          <Route exact path="/" component={StreamList} />
+          <Route path="/streams/new" component={StreamCreate} />
+          <Route path="/streams/edit" component={StreamEdit} />
+          <Route path="/streams/delete" component={StreamDelete} />
+          <Route path="/streams/show" component={StreamShow} />
+        </BrowserRouter>
+      </div>
+    );
+  }
+}
+
+export default App;
