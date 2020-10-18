09b496fbe2323c6a0d08dcb495cfb837a807a3ef
Rohith M <rohith.m94@gmail.com>
2020-10-18 19:54:40 +0530

    integrated redux-form for seamles form mangement

--- client-app/src/components/GoogleAuth.js
+++ client-app/src/components/GoogleAuth.js
@@ -62,7 +62,6 @@ class GoogleAuth extends Component {
 }
 
 const mapStateToProps = (state) => {
-  console.log(state);
   return { isSignedIn: state.auth.isSignedIn };
 };
 
--- client-app/src/components/streams/StreamCreate.js
+++ client-app/src/components/streams/StreamCreate.js
@@ -1,7 +1,72 @@
-import React from "react";
+import React, { Component } from "react";
+import { Field, reduxForm } from "redux-form";
 
-const StreamCreate = () => {
-  return <div>StreamCreate</div>;
+class StreamCreate extends Component {
+  renderError(meta) {
+    if (meta.touched && meta.error) {
+      return (
+        <div className="ui error message">
+          <div className="header">{meta.error}</div>
+        </div>
+      );
+    }
+  }
+
+  renderInput = (formProps) => {
+    const className = `field ${
+      formProps.meta.error && formProps.meta.touched ? "error" : ""
+    }`;
+    return (
+      <div className={className}>
+        <label>{formProps.label}</label>
+        <input {...formProps.input} autoComplete="off" />
+        {/* {formProps.meta.touched && <div>{formProps.meta.error}</div>}  */}
+        {this.renderError(formProps.meta)}
+      </div>
+    );
+  };
+
+  onSubmit(formValues) {
+    console.log(formValues);
+  }
+
+  render() {
+    return (
+      <div>
+        <form
+          className="ui form error"
+          onSubmit={this.props.handleSubmit(this.onSubmit)}
+        >
+          <Field
+            name="title"
+            component={this.renderInput}
+            label="Enter Title"
+          />
+          <Field
+            name="description"
+            component={this.renderInput}
+            label="Enter description"
+          />
+          <button className="ui button primary">submit</button>
+        </form>
+      </div>
+    );
+  }
+}
+
+const validate = (formValues) => {
+  const errors = {};
+  if (!formValues.title) {
+    errors.title = "Enter Title";
+  }
+
+  if (!formValues.description) {
+    errors.description = "Description cannot be empty";
+  }
+
+  return errors;
 };
 
-export default StreamCreate;
+export default reduxForm({ form: "streamCreate", validate: validate })(
+  StreamCreate
+);
